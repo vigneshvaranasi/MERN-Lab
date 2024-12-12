9e54e9e85247f910835b449f5cbf94d7ba43223e
vigneshvaranasi <varanasivsv@gmail.com>
2024-12-12 11:59:10 +0530

    first commit

---
+++ 01-College-Site/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('navbar', () => ({
+        open: false,
+        toggle() {
+            this.open = !this.open;
+        },
+        close() {
+            this.open = false;
+        }
+    }));
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+# MERN-Lab
+This repository contains the lab exercises for the MERN stack course.
+
+1. [Re Design & Develop College Site](https://vigneshvaranasi.github.io/MERN-Lab/01-College-Site/)
